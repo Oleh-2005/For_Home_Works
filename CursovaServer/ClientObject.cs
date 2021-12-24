b29aaf82238a7f79ccb3db37e53af4e13e2f8b80
Oleh <helgi2000000@gmail.com>
2021-12-24 15:48:48 +0200

    Course project

---
+++ CursovaServer/ClientObject.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursovaServer
+{
+    class ClientObject
+    {
+        public string Id { get; private set; }
+        public NetworkStream Stream { get; private set; }
+        string userName;
+        TcpClient client;
+        ServerObject server; 
+
+        public ClientObject(TcpClient tcpClient, ServerObject serverObject)
+        {
+            Id = Guid.NewGuid().ToString();
+            client = tcpClient;
+            server = serverObject;
+            serverObject.AddConnection(this);
+        }
+
+        public void Process()
+        {
+            try
+            {
+                Stream = client.GetStream();
+                string message = GetMessage();
+                userName = message;
+
+                message = userName + " connected to the chat";
+                server.BroadcastMessage(message, this.Id);
+                Console.WriteLine(message);
+                while (true)
+                {
+                    try
+                    {
+                        message = GetMessage();
+                        message = $"{userName} : {message}";
+                        Console.WriteLine(message);
+                        server.BroadcastMessage(message, this.Id);
+                    }
+                    catch
+                    {
+                        message = String.Format("{0} : leave the chat", userName);
+                        Console.WriteLine(message);
+                        server.BroadcastMessage(message, this.Id);
+                        break;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                server.RemoveConnection(this.Id);
+                Close();
+            }
+        }
+
+        public string GetMessage()
+        {
+            byte[] data = new byte[64];
+            StringBuilder builder = new StringBuilder();
+            int bytes = 0;
+            do
+            {
+                bytes = Stream.Read(data, 0, data.Length);
+                builder.Append(Encoding.Unicode.GetString(data, 0, bytes));
+            }
+            while (Stream.DataAvailable);
+
+            return builder.ToString();
+        }
+
+        
+        public void Close()
+        {
+            if (Stream != null)
+                Stream.Close();
+            if (client != null)
+                client.Close();
+
+        }
+    }
+}
---
+++ CursovaServer/ServerObject.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CursovaServer
+{
+    class ServerObject
+    {
+        public TcpListener tcpListener; 
+        List<ClientObject> clients = new List<ClientObject>(); 
+
+        public void AddConnection(ClientObject clientObject)
+        {
+            clients.Add(clientObject);
+        }
+        public void RemoveConnection(string id)
+        {
+            ClientObject client = clients.FirstOrDefault(c => c.Id == id);
+            if (client != null)
+            {
+                clients.Remove(client);
+            }
+        }
+        public void Listen()
+        {
+            try
+            {
+                tcpListener = new TcpListener(IPAddress.Any, 8888);
+                tcpListener.Start();
+                Console.WriteLine("Server has been started . Wait for connection ... ");
+
+                while (true)
+                {
+
+                    TcpClient tcpClient = tcpListener.AcceptTcpClient();
+
+                    ClientObject clientObject = new ClientObject(tcpClient, this);
+                    Thread clientThread = new Thread(new ThreadStart(clientObject.Process));
+                    clientThread.Start();
+                }
+                
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Disconnect();
+            }
+        }
+
+        public void BroadcastMessage(string message, string id)
+        {
+            byte[] data = Encoding.Unicode.GetBytes(message);            
+            foreach (var item in clients)
+            {
+                if(item.Id != id)
+                {
+                    item.Stream.Write(data, 0, data.Length);
+                }
+            }
+        }
+        public void Disconnect()
+        {
+            tcpListener.Stop(); 
+
+            for (int i = 0; i < clients.Count; i++)
+            {
+                clients[i].Close(); 
+            }
+            Environment.Exit(0); 
+        }
+    }
+}
